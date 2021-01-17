c60a66c88c89a41307e0095fbbfdf2ae3a72416a
Joshua Theeuf <joshua.theeuf@moneyloop.com.au>
2021-01-17 22:31:48 +1100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :indices
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'indices#index'
+  root 'indices#home'
   get "index", to:  "indices#index"
   get 'home', to: 'indices#home'
 
