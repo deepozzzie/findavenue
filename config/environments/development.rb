0dded9e5cf4a91c6afcf7620cea905e7c9a7daaa
Joshua Theeuf <joshua.theeuf@moneyloop.com.au>
2021-01-18 10:57:06 +1100

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   config.hosts << "ec2-54-252-170-18.ap-southeast-2.compute.amazonaws.com"
+  config.hosts << "demo.findavenue.io"
 
   # Do not eager load code on boot.
   config.eager_load = false
