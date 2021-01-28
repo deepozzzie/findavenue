3b6af28a964479d204296dae9c2bfc93e80566d2
Joshua Theeuf <joshua.theeuf@moneyloop.com.au>
2021-01-28 11:44:31 +1100

    Update patron.rb

--- app/models/patron.rb
+++ app/models/patron.rb
@@ -33,7 +33,6 @@ class Patron < ApplicationRecord
   def text_waitlist(current_user)
     @venue = current_user.company
     @max_to_invite = (@venue.capacity.to_i - (@venue.capacity.to_i*(@venue.percentage_full/100)))
-    byebug
     @patrons = Patron.all.where(waitlist: true, company_id: @venue.id, created_at:  (Time.now - 3.hours)..Time.now).order(created_at: :desc).limit(@max_to_invite)
     @patrons.each do |p|
       if (@venue.capacity.to_i > (@venue.capacity.to_i*(@venue.percentage_full/100)))
